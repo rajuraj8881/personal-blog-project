bdf0e26fb54b8b7667da4e58576b6a2c53589025
rajumon <raju.mondal@winexsoft.com>
2020-12-27 20:41:32 +0600

    complete registraion par

--- src/index.php
+++ src/index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <div>
+  <div class="regi-form">
     <form action="process.php" method="post">
       <label>Name:</label>
       <input type="text"name="name"><br><br>
