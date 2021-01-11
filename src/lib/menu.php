f1bf2c2c205e3544c5ccf64c587462bc1233caf7
rajumon <raju.mondal@winexsoft.com>
2021-01-12 00:07:32 +0600

    comment page merge issue

--- src/lib/menu.php
+++ src/lib/menu.php
@@ -2,6 +2,7 @@
 <ul>
 <li><a href="dashboard.php">Dashboard</a></li>
 <li><a href="allpost.php">All Post</a></li>
+<li><a href="dashboard.php">My Post</a></li>
 <li><a href="logout.php">Logout</a></li>
 </ul>
 </div>
