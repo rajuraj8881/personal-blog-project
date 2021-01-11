812d7f9a6910195aa41e4615c72dc678279633bf
rajumon <raju.mondal@winexsoft.com>
2021-01-12 02:35:12 +0600

    show all post

--- src/lib/menu.php
+++ src/lib/menu.php
@@ -1,8 +1,8 @@
 <div class="menu">
 <ul>
-<li><a href="dashboard.php">Dashboard</a></li>
-<li><a href="allpost.php">All Post</a></li>
 <li><a href="dashboard.php">My Post</a></li>
+<li><a href="allpost.php">All Post</a></li>
+<li><a href="addpost.php?id=<?php echo $uid;?>">Add Post</a></li>
 <li><a href="logout.php">Logout</a></li>
 </ul>
 </div>
