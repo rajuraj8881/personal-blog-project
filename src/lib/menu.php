4a9bbd81bcfb0ee9a70cf7befa65a2026e043ced
rajumon <raju.mondal@winexsoft.com>
2021-01-23 03:25:27 +0600

    active menu item

--- src/lib/menu.php
+++ src/lib/menu.php
@@ -1,13 +1,13 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="col-md-2"></div>
-    <div class="col-md-8">
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="dashboard.php">My Post</a></li>
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="allpost.php">All Post</a></li>
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="addpost.php">Add Post</a></li>
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="logout.php">Logout</a></li>
-            </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="dashboard.php">My Post</a></li>
+                    <li class="nav-item"><a class="nav-link" aria-current="page" href="allpost.php">All Post</a></li>
+                    <li class="nav-item"><a class="nav-link" aria-current="page" href="addpost.php">Add Post</a></li>
+                    <li class="nav-item"><a class="nav-link" aria-current="page" href="logout.php">Logout</a></li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
