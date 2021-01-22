5948279e4a97c5a4838d6b5f315c403fd1405929
rajumon <raju.mondal@winexsoft.com>
2021-01-23 03:53:52 +0600

    add search menubar

--- src/lib/menu.php
+++ src/lib/menu.php
@@ -1,6 +1,6 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="col-md-2"></div>
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="dashboard.php">My Post</a></li>
@@ -8,6 +8,17 @@
                     <li class="nav-item"><a class="nav-link" aria-current="page" href="addpost.php">Add Post</a></li>
                     <li class="nav-item"><a class="nav-link" aria-current="page" href="logout.php">Logout</a></li>
                 </ul>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="input-group rounded">
+                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
+                    aria-describedby="search-addon" />
+                <span class="input-group-text border-0" id="search-addon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </span>
             </div>
         </div>
     </nav>
