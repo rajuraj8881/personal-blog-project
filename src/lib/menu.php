9060eb6a76d9f4e0f1d98adaf549f5162c7d1059
rajumon <raju.mondal@winexsoft.com>
2021-01-23 00:10:27 +0600

    navbar disgn

--- src/lib/menu.php
+++ src/lib/menu.php
@@ -1,8 +1,13 @@
-<div class="menu">
-<ul>
-<li><a href="dashboard.php">My Post</a></li>
-<li><a href="allpost.php">All Post</a></li>
-<li><a href="addpost.php">Add Post</a></li>
-<li><a href="logout.php">Logout</a></li>
-</ul>
-</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="dashboard.php">My Post</a></li>
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="allpost.php">All Post</a></li>
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="addpost.php">Add Post</a></li>
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
