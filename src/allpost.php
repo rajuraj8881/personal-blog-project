01375d776a074951b06234809e91f0586ff8c5b9
rajumon <raju.mondal@winexsoft.com>
2021-01-24 02:06:19 +0600

    pagination section design

--- src/allpost.php
+++ src/allpost.php
@@ -43,6 +43,21 @@
                         ?>
                     </tbody> <!--End table body -->
                 </table>
+                <nav aria-label="">
+                    <ul class="pagination">
+                        <li class="page-item disabled">
+                            <a class="page-link" href="#" tabindex="-1">Previous</a>
+                        </li>
+                        <li class="page-item active"><a class="page-link" href="#">1 <span class="sr-only">(current)</span></a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">2</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">Next</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </div>
