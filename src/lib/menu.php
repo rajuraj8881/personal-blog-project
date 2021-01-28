49e2c409dad1033da12c4c0ada55a171578c7c7e
Raju Mondal <45078493+rajuraj8881@users.noreply.github.com>
2021-01-28 18:02:22 +0600

    profile page design

--- src/lib/menu.php
+++ src/lib/menu.php
@@ -6,6 +6,7 @@
                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="dashboard.php">My Post</a></li>
                     <li class="nav-item"><a class="nav-link" aria-current="page" href="allpost.php">All Post</a></li>
                     <li class="nav-item"><a class="nav-link" aria-current="page" href="addpost.php">Add Post</a></li>
+                    <li class="nav-item"><a class="nav-link" aria-current="page" href="profile.php">Profile</a></li>
                     <li class="nav-item"><a class="nav-link" aria-current="page" href="logout.php">Logout</a></li>
                 </ul>
             </div>
